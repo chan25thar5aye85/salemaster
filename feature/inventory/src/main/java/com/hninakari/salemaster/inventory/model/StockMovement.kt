980fc5614cc518305300bd243d4bac4deff66eec
Chan Thar <chan25thar5aye85@gmail.com>
2026-08-10 10:58:57 +0630

    feat: add stock movement model

---
+++ feature/inventory/src/main/java/com/hninakari/salemaster/inventory/model/StockMovement.kt
@@ -0,0 +1,19 @@
+package com.hninakari.salemaster.inventory.model
+
+import java.time.Instant
+
+data class StockMovement(
+    val id: Long,
+    val productId: Long,
+    val quantity: Int,
+    val type: StockMovementType,
+    val occurredAt: Instant
+)
+
+enum class StockMovementType {
+    PURCHASE,
+    SALE,
+    RETURN,
+    DAMAGE,
+    ADJUSTMENT
+}
